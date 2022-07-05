10ad36837911179cf09815346c95c878ec971fe8
= <=olegnaop@gmail.com>
2022-07-06 00:00:28 +0300

    fix vite build error with swiper

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    optimizeDeps: {
+        exclude: ["swiper/vue", "swiper/types"],
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
