73358175458ccf6cc42afd1bc8f696afee086f7c
= <=olegnaop@gmail.com>
2022-07-06 17:32:53 +0300

    add shop page

---
+++ src/views/product/products-shop.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import ProductCardGrid from "@layouts/product-card-grid.vue";
+import ProductCard from "@components/product-card.vue";
+import PagePagination from "@components/page-pagination.vue";
+</script>
+<template>
+    <div class="shop-page">
+        <ProductCardGrid>
+            <ProductCard v-for="(item, index) of 12" :key="index" />
+        </ProductCardGrid>
+        <PagePagination />
+    </div>
+</template>
