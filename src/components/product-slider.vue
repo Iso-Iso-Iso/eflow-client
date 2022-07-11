7e573e656efa82bde2918f30745d5b2a5c138e2f
= <=olegnaop@gmail.com>
2022-07-12 00:40:32 +0300

    add slider for single product page

--- src/components/product-slider.vue
+++ src/components/product-slider.vue
@@ -1,87 +1,115 @@
 <script lang="ts" setup>
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Thumbs } from "swiper";
+import { Thumbs, Pagination } from "swiper";
 import { Swiper as SwiperType } from "swiper/types";
 import { ref, watch } from "vue";
 import useSliderZoom from "@composables/use-slider-zoom";
 import LabelButtom from "@components/label-button.vue";
+import PrimaryButton from "@components/primary-button.vue";
 
 const isZoomActive = ref(false);
 const imgSlides = ref<any | null>(null);
-const swiperEntity = ref<SwiperType | null>(null);
+const swiperGalleryEntity = ref<SwiperType | null>(null);
+const swiperThumbsEntity = ref<SwiperType | null>(null);
+const swiperPagination = ref(null);
 
-const onSwiper = (swiper: SwiperType) => (swiperEntity.value = swiper);
+const onGallerySwiper = (swiper: SwiperType) =>
+    (swiperGalleryEntity.value = swiper);
+const onThumbsSwiper = (swiper: SwiperType) =>
+    (swiperThumbsEntity.value = swiper);
 
 watch(isZoomActive, (newValue: boolean) =>
-    swiperEntity.value ? (swiperEntity.value.allowTouchMove = !newValue) : false
+    swiperGalleryEntity.value
+        ? (swiperGalleryEntity.value.allowTouchMove = !newValue)
+        : false
 );
 
 useSliderZoom(imgSlides, {
-    zoomContainerClass: ".prodcut-slider__zoom-container",
-    triggerZoomElementClass: ".prodcut-slider__img",
+    zoomContainerClass: ".prodcut-gallery__zoom-container",
+    triggerZoomElementClass: ".prodcut-gallery__img",
     isZoomActive,
 });
 </script>
+
 <template>
-    <div class="prodcut-slider">
-        <Swiper :modules="[Thumbs]" @swiper="onSwiper">
-            <SwiperSlide
-                v-for="(item, index) of 3"
-                ref="imgSlides"
-                :key="index"
-                class="prodcut-slider__slide"
-                @click="isZoomActive = !isZoomActive"
+    <div class="product-slider-wrapper">
+        <div class="product-thumbnails-wrapper">
+            <PrimaryButton @click="swiperThumbsEntity?.slidePrev()">
+                <BIconArrowUp />
+            </PrimaryButton>
+            <Swiper
+                :slides-per-view="4"
+                :space-between="15"
+                :modules="[Thumbs]"
+                direction="vertical"
+                class="product-thumbnails"
+                @swiper="onThumbsSwiper"
             >
-                <div class="prodcut-slider__zoom-container img">
-                    <img src="@assets/product-card-thumbnail.jpg" alt="" />
+                <SwiperSlide v-for="(item, index) of 7" :key="index">
+                    <div class="product-thumbnails__img img">
+                        <img src="@assets/product-card-thumbnail.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            <PrimaryButton @click="swiperThumbsEntity?.slideNext()">
+                <BIconArrowDown />
+            </PrimaryButton>
+        </div>
+
+        <div class="prodcut-gallery">
+            <Swiper
+                :modules="[Thumbs, Pagination]"
+                :pagination="{ el: swiperPagination }"
+                :thumbs="{ swiper: swiperThumbsEntity }"
+                @swiper="onGallerySwiper"
+            >
+                <SwiperSlide
+                    v-for="(item, index) of 7"
+                    ref="imgSlides"
+                    :key="index"
+                    class="prodcut-gallery__slide"
+                    @click="isZoomActive = !isZoomActive"
+                >
+                    <div class="prodcut-gallery__zoom-container img">
+                        <img src="@assets/product-card-thumbnail.jpg" alt="" />
+                    </div>
+                    <div class="img prodcut-gallery__img">
+                        <img
+                            class=""
+                            src="@assets/product-card-thumbnail.jpg"
+                            alt=""
+                        />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            <div class="flex-space">
+                <Transition
+                    mode="out-in"
+                    name="label-anim"
+                    @click="isZoomActive = !isZoomActive"
+                >
+                    <LabelButtom v-if="!isZoomActive">
+                        <BIconZoomIn /> zoom
+                    </LabelButtom>
+                    <LabelButtom v-else>
+                        <BIconZoomOut />
+                        hover on image
+                    </LabelButtom>
+                </Transition>
+                <div
+                    ref="swiperPagination"
+                    class="prodcut-gallery__pagination"
+                ></div>
+                <div class="flex-center">
+                    <LabelButtom @click="swiperGalleryEntity?.slidePrev()">
+                        <BIconArrowLeft />
+                    </LabelButtom>
+                    <LabelButtom @click="swiperGalleryEntity?.slideNext()">
+                        <BIconArrowRight />
+                    </LabelButtom>
                 </div>
-                <div class="img prodcut-slider__img">
-                    <img
-                        class=""
-                        src="@assets/product-card-thumbnail.jpg"
-                        alt=""
-                    />
-                </div>
-            </SwiperSlide>
-        </Swiper>
-        <Transition
-            mode="out-in"
-            name="label-anim"
-            @click="isZoomActive = !isZoomActive"
-        >
-            <LabelButtom v-if="!isZoomActive">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                    <path
-                        fill-rule="evenodd"
-                        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
-                    />
-                    <path
-                        d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"
-                    />
-                    <path
-                        fill-rule="evenodd"
-                        d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"
-                    />
-                </svg>
-                zoom
-            </LabelButtom>
-            <LabelButtom v-else>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                    <path
-                        fill-rule="evenodd"
-                        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
-                    />
-                    <path
-                        d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"
-                    />
-                    <path
-                        fill-rule="evenodd"
-                        d="M3 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-                    />
-                </svg>
-                close
-            </LabelButtom>
-        </Transition>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="scss" src="./product-slider.scss"></style>
